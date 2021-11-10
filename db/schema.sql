27443380c872bc4d7d5f585acf3988e3f7b27386
cshepscorp <sheppard.christy@gmail.com>
2021-11-09 21:10:13 -0500

    voter table and records added

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
@@ -16,3 +17,13 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
+-- In SQL, a DATETIME field's value will look something like 2020-01-01 13:00:00
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY Key,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- we're specifying CURRENT_TIMESTAMP as the value for DEFAULT
+);
+-- return the current date and time in the same 2020-01-01 13:00:00 format. Note that the time will be based on what time it is according to your server, not the client's machine
