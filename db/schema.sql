81bc0589d221e4549925935760335afe14b74db5
cshepscorp <sheppard.christy@gmail.com>
2021-11-10 20:56:16 -0500

    vote table added

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS voters;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
 CREATE TABLE parties (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    description TEXT
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
--- comment 
+
 
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -20,10 +21,29 @@ CREATE TABLE candidates (
 
 -- In SQL, a DATETIME field's value will look something like 2020-01-01 13:00:00
 CREATE TABLE voters (
-    id INTEGER AUTO_INCREMENT PRIMARY Key,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP -- we're specifying CURRENT_TIMESTAMP as the value for DEFAULT
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+-- we're specifying CURRENT_TIMESTAMP as the value for DEFAULT
+
 -- return the current date and time in the same 2020-01-01 13:00:00 format. Note that the time will be based on what time it is according to your server, not the client's machine
+
+-- this table should prevent duplicate records
+-- if the relevant candidate or voter is removed from the database their vote should be removed
+-- can be done with JavaScript but easier to use CONSTRAINTS
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
+
+-- uc_voter, signifies that the values inserted into the voter_id field must be unique
+-- ON DELETE SET NULL would set the record's field to NULL if the key from the reference table was deleted
+-- ON DELETE CASCADE, deleting the reference key will also delete the entire row from this table
